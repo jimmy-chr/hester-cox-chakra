c2cbbd1ba7ac74c9811244a7fb9d2f1709cf29bc
Jimmy Christiaens <jimmy_chr@hotmail.com>
2024-09-27 12:41:54 +0200

    adding first pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </StrictMode>
 );
