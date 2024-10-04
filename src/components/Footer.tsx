0cceee9d5f82fe6afc34dfd37ee8e3c3a434f093
Jimmy Christiaens <jimmy_chr@hotmail.com>
2024-10-04 17:25:09 +0200

    adding icons and links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,18 +4,21 @@ import { BsInstagram, BsFacebook } from "react-icons/bs";
 const Footer = () => {
   return (
     <Box bg="green.800" p={4}>
-      <Stack direction="row" width={"100%"} justifyContent={"center"}>
-        <Text color="orange.50" textAlign="center">
-          © 2024 Hester Cox. All Rights Reserved.
-        </Text>
-        <Box marginX={5}>
+      <Stack
+        direction="row"
+        width={"100%"}
+        justifyContent={"center"}
+        spacing={10}
+      >
+        <Text color="orange.50">© 2024 Hester Cox. All Rights Reserved.</Text>
+        <Stack direction="row">
           <Link href="https://www.instagram.com/hesterbijdehand/">
             <Icon as={BsInstagram} color="orange.50" />
           </Link>
           <Link href="https://www.facebook.com/hesterbijdehand">
             <Icon as={BsFacebook} color="orange.50" />
           </Link>
-        </Box>
+        </Stack>
       </Stack>
     </Box>
   );
