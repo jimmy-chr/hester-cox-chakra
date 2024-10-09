191d10ab598e1bc6f97694034c06d2547c364e51
Jimmy Christiaens <jimmy_chr@hotmail.com>
2024-10-09 20:44:44 +0200

    adding responsive alignment between header and pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Link, Spacer, Stack } from "@chakra-ui/react";
+import { Box, Flex, Heading, Link, Stack } from "@chakra-ui/react";
 import { LanguageSelect } from "./LanguageSelect";
 import { useTranslation } from "react-i18next";
 import { Link as ReactRouterLink } from "react-router-dom";
@@ -7,26 +7,20 @@ import Navigation from "./Navigation";
 function Header() {
   const { t } = useTranslation();
   return (
-    <Box bg="green.800" p={"1em"} color="orange.50">
+    <Box bg="green.800" p={["1em", "2em"]} color="orange.50">
       <Flex justify="space-between" align="flex-start">
         <Stack direction={"column"}>
-          <Link
-            mx={"1em"}
-            as={ReactRouterLink}
-            to="/"
-            style={{ textDecoration: "none" }}
-          >
-            <Heading as="h1" size="lg" paddingY={"0.5em"}>
+          <Link as={ReactRouterLink} to="/" style={{ textDecoration: "none" }}>
+            <Heading as="h1" size="lg" paddingBottom={"0.5em"}>
               {t("header.title")}
             </Heading>
-            <Heading as="h2" size="md" paddingY={"0.25em"}>
+            <Heading as="h2" size="md">
               {t("header.subtitle")}
             </Heading>
           </Link>
         </Stack>
         <Flex flexDirection="column" align="flex-end">
           <LanguageSelect />
-          <Spacer />
           <Navigation />
         </Flex>
       </Flex>
