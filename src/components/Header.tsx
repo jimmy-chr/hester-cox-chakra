066266ae1fcab59a80887c8e385fef712578411f
Jimmy Christiaens <jimmy_chr@hotmail.com>
2024-10-09 21:03:28 +0200

    starting collection page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Link, Stack } from "@chakra-ui/react";
+import { Box, Flex, Heading, Link, Stack, Center } from "@chakra-ui/react";
 import { LanguageSelect } from "./LanguageSelect";
 import { useTranslation } from "react-i18next";
 import { Link as ReactRouterLink } from "react-router-dom";
@@ -8,22 +8,33 @@ function Header() {
   const { t } = useTranslation();
   return (
     <Box bg="green.800" p={["1em", "2em"]} color="orange.50">
-      <Flex justify="space-between" align="flex-start">
-        <Stack direction={"column"}>
-          <Link as={ReactRouterLink} to="/" style={{ textDecoration: "none" }}>
-            <Heading as="h1" size="lg" paddingBottom={"0.5em"}>
-              {t("header.title")}
-            </Heading>
-            <Heading as="h2" size="md">
-              {t("header.subtitle")}
-            </Heading>
-          </Link>
-        </Stack>
-        <Flex flexDirection="column" align="flex-end">
-          <LanguageSelect />
-          <Navigation />
+      <Center>
+        <Flex
+          justify="space-between"
+          align="flex-start"
+          width={"80em"}
+          maxWidth={"80em"}
+        >
+          <Stack direction={"column"}>
+            <Link
+              as={ReactRouterLink}
+              to="/"
+              style={{ textDecoration: "none" }}
+            >
+              <Heading as="h1" size="lg" paddingBottom={"0.5em"}>
+                {t("header.title")}
+              </Heading>
+              <Heading as="h2" size="md">
+                {t("header.subtitle")}
+              </Heading>
+            </Link>
+          </Stack>
+          <Flex flexDirection="column" align="flex-end">
+            <LanguageSelect />
+            <Navigation />
+          </Flex>
         </Flex>
-      </Flex>
+      </Center>
     </Box>
   );
 }
